ee59712e7e52d991e570d45baa1d5d50a462338a
Pablo Molina <pablomolinarojas@gmail.com>
2025-06-04 20:30:11 -0400

    resume

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/melocoton/Developer/spacecraft/src/app/layout":["static/media/7cba1811e3c25a15-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
